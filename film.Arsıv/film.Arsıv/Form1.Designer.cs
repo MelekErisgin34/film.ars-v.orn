bf6d242fb7ad3ee28ef105c09167515031ba862e
MelekErisgin34 <147922709+MelekErisgin34@users.noreply.github.com>
2024-03-19 11:36:47 +0300

    Add files via upload

---
+++ film.Arsıv/film.Arsıv/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace film.Arsıv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvFilmler = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvFilmler
+            // 
+            this.dgvFilmler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFilmler.Location = new System.Drawing.Point(225, 255);
+            this.dgvFilmler.Name = "dgvFilmler";
+            this.dgvFilmler.Size = new System.Drawing.Size(240, 150);
+            this.dgvFilmler.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvFilmler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmler)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFilmler;
+    }
+}
+
---
+++ film.Arsıv/film.Arsıv/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace film.Arsıv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Server=localhost;Database=film_arsiv;Uid=root;Pwd='';";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (MySqlConnection baglanti = new MySqlConnection(connectionString))
+            {
+                baglanti.Open();
+                string sorgu = "SELECT *FROM filmler";
+                MySqlCommand cmd =new MySqlCommand(sorgu,baglanti);
+                MySqlDataAdapter adapter= new MySqlDataAdapter(cmd);
+                DataTable tablo = new DataTable();
+
+                adapter.Fill(tablo);
+
+                dgvFilmler.DataSource = tablo;
+                
+                
+                   
+
+
+            }
+
+
+        }
+    }
+}
